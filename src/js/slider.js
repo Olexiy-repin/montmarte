606d750ef37fc8a89d0b297e0e6fc78de998fb49
Olexiy <olexiy@iMacDesign.local>
2018-07-04 18:11:41 +0200

    added three section

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,24 @@
+// (() => {
+//   $(".response-slider").owlCarousel({
+//    items : 1,
+//    itemsCustom : true,
+//
+//    loop: true,
+//
+//    autoHeight : false,
+//
+//    margin : 5,
+//    nav: true,
+//    navText: ["<img src='img/sec-06/larr.png'>","<img src='img/sec-06/rarr.png'>"],
+//    dots: false,
+//    animateOut: 'slideOutDown',
+//    animateIn: 'flipInX',
+//    mouseDrag: false
+//   });
+// })();
+
 (() => {
-  $(".response-slider").owlCarousel({
+  $(".cooperation__slider").owlCarousel({
    items : 1,
    itemsCustom : true,
 
@@ -8,28 +27,9 @@
    autoHeight : false,
 
    margin : 5,
-   nav: true,
-   navText: ["<img src='img/sec-06/larr.png'>","<img src='img/sec-06/rarr.png'>"],
-   dots: false,
-   animateOut: 'slideOutDown',
-   animateIn: 'flipInX',
-   mouseDrag: false
-  });
-})();
-
-(() => {
-  $(".sec-12__slider").owlCarousel({
-   items : 1,
-   itemsCustom : true,
-
-   loop: true,
-
-   autoHeight : false,
-
-   margin : 5,
-   nav: true,
-   navText: ["<img src='img/sec-06/larr.png'>","<img src='img/sec-06/rarr.png'>"],
-   dots: false,
+   nav: false,
+   navText: ["",""],
+   dots: true,
 
    responsiveClass:true,
    responsive:{
@@ -51,38 +51,52 @@
        }
    }
   });
-})();
 
-(() => {
-  $(".sec-14__slider").owlCarousel({
-   items : 1,
-   itemsCustom : true,
+$(".cooperation__slider").on('changed.owl.carousel', function (event) {
+    let index = event.item.index;
 
-   loop: true,
-
-   autoHeight : false,
-
-   margin : 5,
-   nav: true,
-   navText: ["<img src='img/sec-06/larr.png'>","<img src='img/sec-06/rarr.png'>"],
-   dots: false,
-
-   responsiveClass:true,
-   responsive:{
-       0:{
-           items:1,
-           nav:true
-       },
-       768:{
-           items:1,
-           nav:true,
-           margin: 10,
-       },
-       1200:{
-           items:1,
-           nav:true,
-           margin: 20
-       }
-   }
+    if (index == 3) {
+      $('.cooperation__slider-count').text('02');
+    } else if (index == 2) {
+      $('.cooperation__slider-count').text('01');
+    } else if (index == 4) {
+      $('.cooperation__slider-count').text('03');
+    } else if (index == 5) {
+      $('.cooperation__slider-count').text('01');
+    }
   });
 })();
+
+// (() => {
+//   $(".sec-14__slider").owlCarousel({
+//    items : 1,
+//    itemsCustom : true,
+//
+//    loop: true,
+//
+//    autoHeight : false,
+//
+//    margin : 5,
+//    nav: true,
+//    navText: ["<img src='img/sec-06/larr.png'>","<img src='img/sec-06/rarr.png'>"],
+//    dots: false,
+//
+//    responsiveClass:true,
+//    responsive:{
+//        0:{
+//            items:1,
+//            nav:true
+//        },
+//        768:{
+//            items:1,
+//            nav:true,
+//            margin: 10,
+//        },
+//        1200:{
+//            items:1,
+//            nav:true,
+//            margin: 20
+//        }
+//    }
+//   });
+// })();
